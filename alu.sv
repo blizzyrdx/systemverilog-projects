fb2170c65211517de6c971be2502f7472cf2ebbf
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-08-22 00:38:34 -0700

    challenge 1

--- alu.sv
+++ alu.sv
@@ -20,5 +20,22 @@ typedef enum logic [1:0] {RED, GREEN, YELLOW} state_t;
         end //end to always_ff block
     endmodule
 
-    //chalenge 1 
-    
+// Challenge 1: Update the state_t enum to include an additional state called ALL_RED, which represents a state where all traffic lights are red. This will require modifying the enum definition and updating the state transition logic in the always_ff block to include this new state. 
+typedef enum logic [1:0] {RED, GREEN, YELLOW, ALL_RED} state_t; 
+
+    module traffic_light ( //module definition for traffic light controller
+        input logic clk, //clock input 0 and 1s
+        input logic rst, //active low reset input
+        output state_t state
+    );
+        always_ff @(posedge clk) begin // always_ff means flip-flop behavior, triggered on the positive edge of the clock 
+            if (rst)                // posedge means the block will execute on the rising edge of the clock signal, which is a common practice in synchronous digital design
+                state <= RED;
+            else case (state)
+                RED: state <= GREEN;
+                GREEN: state <= YELLOW;
+                YELLOW: state <= ALL_RED;
+                ALL_RED: state <= RED;
+            endcase
+        end //end to always_ff block
+    endmodule
