cc889bd6fd4b1f600904b4cca1e3155249a0466e
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-08-22 00:28:31 -0700

    updated correctly syntax

--- alu.sv
+++ alu.sv
@@ -5,17 +5,20 @@ typedef enum logic [1:0] {RED, GREEN, YELLOW} state_t;
 
     module traffic_light ( //module definition for traffic light controller
         input logic clk, //clock input 0 and 1s
-        input logic rst_n, //active low reset input
+        input logic rst, //active low reset input
         output state_t state
     );
         always_ff @(posedge clk) begin // always_ff means flip-flop behavior, triggered on the positive edge of the clock 
-            if (!rst_n)                // posedge means the block will execute on the rising edge of the clock signal, which is a common practice in synchronous digital design
-                current_state <= RED;
+            if (rst)                // posedge means the block will execute on the rising edge of the clock signal, which is a common practice in synchronous digital design
+                state <= RED;
             else case (state)
-                RED: current_state <= GREEN;
-                GREEN: current_state <= YELLOW;
-                YELLOW: current_state <= RED;
-                default: current_state <= RED;
+                RED: state <= GREEN;
+                GREEN: state <= YELLOW;
+                YELLOW: state <= RED;
+                default: state <= RED;
             endcase
         end //end to always_ff block
     endmodule
+
+    //chalenge 1 
+    
