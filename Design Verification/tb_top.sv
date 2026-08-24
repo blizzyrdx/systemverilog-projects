10e7c969990dc2549ef5ea54e96d8a90f7a23296
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-08-24 10:44:06 -0700

    research UVM

--- Design Verification/tb_top.sv
+++ Design Verification/tb_top.sv
@@ -1,23 +1,48 @@
-// All verification components are placed in this top testbench module
-module tb_top;
+// Top level testbench contains the interface, DUT and test handles which
+// can be used to start test components once the DUT comes out of reset. Or
+// the reset can also be a part of the test class in which case all you need
+// to do is start the test's run method.
+module tb;
+  reg clk;
 
-	// Declare variables that need to be connected to the design instance
-	// These variables are assigned some values that in turn gets transferred to
-	// the design as inputs because they are connected with the ports in the design
-	reg clk;
-	wire en;
-	wire wr;
-	wire data;
+  always #10 clk = ~clk;
+  reg_if _if (clk);
 
-	// Instantiate the design module and connect the variables declared above
-	// with the ports in the design
-	design myDsn ( .clk (clk),
-	               .en  (en),
-	               .wr  (wr),
-	               . ...
-	               .rdata);
+  reg_ctrl u0 ( .clk (clk),
+            	.addr (_if.addr),
+               	.rstn(_if.rstn),
+            	.sel  (_if.sel),
+               	.wr (_if.wr),
+            	.wdata (_if.wdata),
+            	.rdata (_if.rdata),
+            	.ready (_if.ready));
 
-	// Develop rest of the testbench and write stimulus that can be driven to the design
+  initial begin
+    new_test t0;
+
+    clk <= 0;
+    _if.rstn <= 0;
+    _if.sel <= 0;
+    #20 _if.rstn <= 1;
+
+    t0 = new;
+    t0.e0.vif = _if;
+    t0.run();
+
+    // Once the main stimulus is over, wait for some time
+    // until all transactions are finished and then end
+    // simulation. Note that $finish is required because
+    // there are components that are running forever in
+    // the background like clk, monitor, driver, etc
+    #200 $finish;
+  end
+
+  // Simulator dependent system tasks that can be used to
+  // dump simulation waves.
+  initial begin
+    $dumpvars;
+    $dumpfile("dump.vcd");
+  end
 endmodule
 
 // DUT is the design under test, which is typically used in post-silicon verification.
